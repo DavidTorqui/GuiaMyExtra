0bfffaff83b28cf43a9b097ddc0da38b65c072e2
CHCORP\atorresq <adtq15@gmail.com>
2018-10-24 14:57:19 -0500

    GuiaMyExtra Terminada con Screen Play

---
+++ src/test/java/GuiaMyExtra/RunnerTags/RunnerTags.java
@@ -0,0 +1,16 @@
+package GuiaMyExtra.RunnerTags;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+		features="src/test/resources/features/Formulario.feature",
+		tags= "@tag",
+		glue="Reto1.StepDefinitions",
+		snippets=SnippetType.CAMELCASE		)
+public class RunnerTags {
+}
